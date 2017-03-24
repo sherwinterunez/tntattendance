4be54cfc6920c64353eddfbf8fd2b0ff7189211f
Sherwin Terunez <sherwinterunez@Sherwins-MacBook-Air.local>
2017-03-24 16:09:07 +0800

    commit march 24, 2017 16:07

--- templates/default/tapheader.tpl.php
+++ templates/default/tapheader.tpl.php
@@ -268,7 +268,7 @@ do_action('action_header_bottom');
 		height: auto;
 		float: left;
 		overflow: hidden;
-		border: 1px solid #f00;
+		border: 1px solid #ccc;
 		/*margin: 5px;*/
 	}
 
@@ -371,9 +371,24 @@ do_action('action_header_bottom');
 				<div id="studentprevlabel">&nbsp;</div>
 				<br class="br" />
 			</div>
-			<div id="studentprev" class="studentprev"></div>
-			<div id="studentprev" class="studentprev"></div>
-			<div id="studentprev" class="studentprev"></div>
+			<div id="studentprev" class="studentprev">
+				<div style="display:block;width:150px;height:150px;">&nbsp;</div>
+				<div id="studentprevlabel">&nbsp;<br />&nbsp;<br /></div>
+				<div id="studentprevlabel">&nbsp;</div>
+				<br class="br" />
+			</div>
+			<div id="studentprev" class="studentprev">
+				<div style="display:block;width:150px;height:150px;">&nbsp;</div>
+				<div id="studentprevlabel">&nbsp;<br />&nbsp;<br /></div>
+				<div id="studentprevlabel">&nbsp;</div>
+				<br class="br" />
+			</div>
+			<div id="studentprev" class="studentprev">
+				<div style="display:block;width:150px;height:150px;">&nbsp;</div>
+				<div id="studentprevlabel">&nbsp;<br />&nbsp;<br /></div>
+				<div id="studentprevlabel">&nbsp;</div>
+				<br class="br" />
+			</div>
 			<br style="clear:both;" />
 		</div>
 	</div>
