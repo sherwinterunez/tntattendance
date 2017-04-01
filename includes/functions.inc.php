bb78957ed67b703f98c3ef555c54eb1833cb9554
Sherwin Terunez <sherwinterunez@Sherwins-MacBook-Air.local>
2017-04-02 07:47:30 +0800

    commit April 2, 2017 7:50

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -768,6 +768,44 @@ function computeHash($user,$pass) {
 	return sha1(base64_encode($user) . base64_encode($pass));
 }
 
+/**
+ * Retrieve time from an NTP server
+ *
+ * @param    string   $host   The NTP server to retrieve the time from
+ * @return   int      The current unix timestamp
+ * @author   Aidan Lister <aidan@php.net>
+ * @link     http://aidanlister.com/2010/02/retrieve-time-from-an-ntp-server/
+ */
+function getNtpTime($host=false) {
+
+	if(!empty($host)) {
+	} else {
+		$host = '0.centos.pool.ntp.org';
+	}
+
+  // Create a socket and connect to NTP server
+  $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+  socket_connect($sock, $host, 123);
+
+  // Send request
+  $msg = "\010" . str_repeat("\0", 47);
+  socket_send($sock, $msg, strlen($msg), 0);
+
+  // Receive response and close socket
+  socket_recv($sock, $recv, 48, MSG_WAITALL);
+  socket_close($sock);
+
+  // Interpret response
+  $data = unpack('N12', $recv);
+  $timestamp = sprintf('%u', $data[9]);
+
+  // NTP is number of seconds since 0000 UT on 1 January 1900
+  // Unix time is seconds since 0000 UT on 1 January 1970
+  $timestamp -= 2208988800;
+
+  return $timestamp;
+}
+
 function myExec($cmd, &$out = null) {
 	$desc = array(
 			0 => array("pipe", "r"),
